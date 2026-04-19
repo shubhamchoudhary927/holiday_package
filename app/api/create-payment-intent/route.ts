087a6bf366cd80cd341bae297baf371431b464b9
shubhamchoudhary927 <shubhamchoudhary6261@gmail.com>
2026-04-19 09:02:40 +0530

    new

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { prisma } from "@/lib/prisma";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-11-20.acacia",
+  apiVersion: "2026-02-25.clover",
 });
 
 export async function POST(request: Request) {
